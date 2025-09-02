316d9b17093fecdd74dfcc4991842b9b4ea9949c
agustinFlw <hz.agustin10@gmail.com>
2025-09-02 20:50:02 -0300

    Corregir MotoState_NetCore para que sea una carpeta normal

---
+++ MotoState_NetCore/Pages/MotosEnTaller.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MotoState__.NetCore_Razor.Pages
+{
+    public class MotosEnTallerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
